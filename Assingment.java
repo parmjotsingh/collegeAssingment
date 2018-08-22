e6d586b465727e743935a5a323d02d7411e79862
Parmjot <Parmjot03@gmail.com>
2018-08-23 05:29:36 +0530

    First commit

---
+++ Assingment.java
@@ -0,0 +1,19 @@
+sol 1 ->
+
+public class Hello
+{
+	public static void main(String args[])
+	{
+		System.out.println("HELLO WORLD!");
+	}
+}
+====================================================
+sol 2 ->
+
+public class Name
+{
+	public static void main(String args[])
+	{
+		System.out.println("ABC XYZ");
+	}
+}
